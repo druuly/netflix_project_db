4161bba672774fdbbb9b014fdaee2f7962a939d0
oscar <oscareucedaf1@gmail.com>
2026-04-05 15:21:41 -0400

    syntax fixes, create db

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def init_db():
                         FOREIGN KEY (content_id) REFERENCES content (content_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS episode
-                        (episode_id PRIMARY KEY INTEGER AUTOINCREMENT,
+                        (episode_id INTEGER PRIMARY KEY AUTOINCREMENT,
                         season_id INTEGER,
                         episode_number INTEGER,
                         title TEXT,
@@ -39,11 +39,11 @@ def init_db():
                         FOREIGN KEY (season_id) REFERENCES season (season_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS my_list
-                        (list_item_id PRIMARY INTEGER KEY AUTOINCREMENT,
+                        (list_item_id INTEGER PRIMARY KEY AUTOINCREMENT,
                         profile_id INTEGER,
                         content_id INTEGER,
                         added_at INTEGER,
-                        FOREIGN KEY (profile_id) REFERENCES profile (profile_id))''')
+                        FOREIGN KEY (profile_id) REFERENCES profile(profile_id))''')
     
     # here i leave is_original as an integer to abide by SQLite; 1 or 0 is bool
     cursor.execute ('''CREATE TABLE IF NOT EXISTS content
@@ -66,8 +66,8 @@ def init_db():
                         resolution INTEGER,
                         file_url TEXT,
                         codec TEXT,
-                        FOREIGN KEY (content_id) REFERENCES content (content_id)
-                        FOREIGN KEY (episode_id) REFERENCES episode (episde_id))''')
+                        FOREIGN KEY (content_id) REFERENCES content (content_id),
+                        FOREIGN KEY (episode_id) REFERENCES episode (episode_id))''')
     
     # NOTE
     # this is a junction table. notice the syntax
@@ -76,11 +76,11 @@ def init_db():
                         genre_id INTEGER,
                         PRIMARY KEY (content_id , genre_id),
                         FOREIGN KEY (content_id) REFERENCES content (content_id),
-                        FOREIGN KEY (genre_id) REFERENCES genre(genre_id)''')
+                        FOREIGN KEY (genre_id) REFERENCES genre(genre_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS genre
                         (genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        genre_name INTEGER)''')
+                        genre_name TEXT)''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS rating
                         (rating_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -88,53 +88,54 @@ def init_db():
                         content_id INTEGER,
                         rating_value INTEGER,
                         rated_at INTEGER,
-                        FOREIGN KEY (profile_id) REFERENCES profile(profile_id)
-                        FOREIGN KEY (content_id) REFERENCES content(content_id)''')
+                        FOREIGN KEY (profile_id) REFERENCES profile(profile_id),
+                        FOREIGN KEY (content_id) REFERENCES content(content_id))''')
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS studio
                     (studio_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     studio_name TEXT,
-                    country TEXT))''')
+                    country TEXT)''')
     
-    cursor.execute('''CREATE TABLE IF NOT EXISTS content_person,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS content_person
                     (content_id INTEGER,
                     person_id INTEGER,
                     role_type TEXT,
-                    PRIMARY KEY (content_id , person_id)
-                    FOREIGN KEY (content_id) REFERENCES content(content_id)
+                    PRIMARY KEY (content_id , person_id),
+                    FOREIGN KEY (content_id) REFERENCES content(content_id),
                     FOREIGN KEY (person_id) REFERENCES person (person_id))''')
     
     cursor.execute('''CREATE TABLE IF NOT EXISTS person
-                    (person_id INTEGER PRIMARY KEY AUTOINCREMENT
-                    full_name INTEGER,
+                    (person_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    full_name TEXT,
                     birth_date INTEGER,
-                    nationality TEXT))''')
+                    nationality TEXT)''')
     
     cursor.execute('''CREATE TABLE IF NOT EXISTS profile
-                    (profile_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (profile_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     account_id INTEGER,
+                    language_id INTEGER,
                     profile_name TEXT,
                     avatar_url TEXT,
                     is_kids INTEGER,
                     maturity_rating TEXT,
-                    FOREIGN KEY (account_id) REFERENCES account (account_id)
+                    FOREIGN KEY (account_id) REFERENCES account (account_id),
                     FOREIGN KEY (language_id) REFERENCES language(language_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS content_track
-                    (track_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (track_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     content_id INTEGER,
                     language_id INTEGER
                     track_type TEXT,
-                    FOREIGN KEY (content_id) REFERENCES content(content_id)
-                    FOREIGN KEY (languange_id) REFERENCES language(language_id))''')
+                    FOREIGN KEY (content_id) REFERENCES content(content_id),
+                    FOREIGN KEY (language_id) REFERENCES language(language_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS language
-                    (language_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (language_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     language_name TEXT,
-                    iso_code INTEGER))''')
+                    iso_code TEXT)''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS device
-                    (device_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (device_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     account_id INTEGER,
                     device_type TEXT,
                     device_name TEXT,
@@ -142,7 +143,7 @@ def init_db():
                     FOREIGN KEY (account_id) REFERENCES account(account_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS account
-                    (account_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (account_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     email TEXT,
                     password_hash TEXT,
                     created_at INTEGER,
@@ -153,11 +154,11 @@ def init_db():
                     FOREIGN KEY (plan_id) REFERENCES subscription_plan(plan_id))''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS subscription_plan
-                    (plan_id INTEGER PRIMARY KEY AUTOINCREMENT
+                    (plan_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     plan_name TEXT,
                     price_monthly INTEGER,
                     max_profiles INTEGER,
-                    max_simultaneous_streams INTEGER))''')
+                    max_simultaneous_streams INTEGER)''')
     
     cursor.execute ('''CREATE TABLE IF NOT EXISTS payment
                     (payment_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -166,6 +167,13 @@ def init_db():
                     payment_date INTEGER,
                     payment_method TEXT,
                     status TEXT,
-                    plan_id INTEGER
+                    plan_id INTEGER,
                     FOREIGN KEY (account_id) REFERENCES account(account_id)
-                    FOREIGN KEY (plan_id) REFERENCES subscription_plan(plan_id)''')
+                    FOREIGN KEY (plan_id) REFERENCES subscription_plan(plan_id))''')
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
+    print ("Database created.")
